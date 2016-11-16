e065c80050e9053dff15db48bde3fe97a6fe52e8
Mader <torben.mader@adesso.local>
2016-11-16 18:28:38 +0100

    Changes app.js etc

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,1 +1,8 @@
 var summitApp = angular.module('summitApp',['ngRoute']);
+
+summitApp.config(function($routeProvider) {
+  $routeProvider.when('/about', {
+    controller: 'DefCtrl',
+    templateUrl:'view/about.html'
+  })
+});
